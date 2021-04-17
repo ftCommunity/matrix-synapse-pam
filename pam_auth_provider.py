9e596ff94541ff0d207fab2f65e4a81f7ab85327
Richard Kunze <richard.kunze@web.de>
2021-04-17 22:42:42 +0200

    Convert to async/await

--- pam_auth_provider.py
+++ pam_auth_provider.py
@@ -16,7 +16,6 @@
 import pam
 
 from collections import namedtuple
-from twisted.internet import defer
 from synapse import types
 
 class PAMAuthProvider:
@@ -24,16 +23,16 @@ class PAMAuthProvider:
         self.account_handler = account_handler
         self.create_users = config.create_users
 
-    @defer.inlineCallbacks
-    def check_password(self, user_id, password):
+    async def check_password(self, user_id, password):
         """ Attempt to authenticate a user against PAM
             and register an account if none exists.
 
             Returns:
-                True if authentication against PAM was successful
+                True if authentication against PAM was successful,
+                False if not
         """
         if not password:
-            defer.returnValue(None)
+            return False
         # user_id is of the form @foo:bar.com
         localpart = user_id.split(":", 1)[0][1:]
 
@@ -41,21 +40,21 @@ class PAMAuthProvider:
         # If not, bail out without even checking PAM because
         # we can't create a user with that id anyway.
         if types.contains_invalid_mxid_characters(localpart):
-            defer.returnValue(None)
+            return False
 
         # Now check the password
         if not pam.pam().authenticate(localpart, password, service='matrix-synapse'):
-            defer.returnValue(None)
+            return False
 
         # From here on, the user is authenticated
-        if (yield self.account_handler.check_user_exists(user_id)):
-            defer.returnValue(user_id)
+        if (await self.account_handler.check_user_exists(user_id)):
+            return True
 
         if self.create_users:
-            user_id = yield self.account_handler.register_user(localpart=localpart)
-            defer.returnValue(user_id)
+            user_id = await self.account_handler.register_user(localpart=localpart)
+            return bool(user_id)
 
-        defer.returnValue(None)
+        return False
 
     @staticmethod
     def parse_config(config):
